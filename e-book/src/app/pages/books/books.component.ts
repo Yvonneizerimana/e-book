a586a50f7d3cb222c592bf5c4d0ff75c56ee53bf
Yvonneizerimana <izerimanayvonne510@gmail.com>
2024-09-25 17:05:05 +0200

    i'm dealing with update

--- e-book/src/app/pages/books/books.component.ts
+++ e-book/src/app/pages/books/books.component.ts
@@ -15,4 +15,16 @@ books=[
   {id:3,title:"Ngiye Nzahindukira",author:"Hemgoe",description:"proverb of ngiye nzahindukira",price:190000,category:"proverb",uploadedBook:"mbegaa"},
   {id:4,title:"Inkuru ya Manzi",author:"Hemgoe",description:"ibyabaye kuri manzi igihe yaratangiye ishuri",price:78000,category:"Inkuru Mpimbano",uploadedBook:"so good and cool"},
 ]
+
+deleteBook(id:number){
+  this.books = this.books.filter(book=>book.id!=id)
+  console.log("book deleted successfuly",this.books)
+  alert("Book deleted successfully")
+}
+
+updateBook(id:number){
+ let book=this.books.find(book=>book.id==id)
+ if(book){
+  book.title=""
+ }
 }
